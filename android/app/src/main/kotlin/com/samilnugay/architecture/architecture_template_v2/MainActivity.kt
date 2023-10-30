58f7853922a2c3f3dd794872c5fc270c30b546c7
Şamil Nugay <samil_nugay@hotmail.com>
2023-10-30 20:56:41 +0300

    creating the folder structure with project and module creation

---
+++ android/app/src/main/kotlin/com/samilnugay/architecture/architecture_template_v2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.samilnugay.architecture.architecture_template_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/feature/home/view/mixin/home_view_mixin.dart
@@ -0,0 +1,6 @@
+import 'package:architecture_template_v2/feature/home/home_view.dart';
+import 'package:flutter/material.dart';
+
+mixin HomeViewMixin on State<HomeView> {
+  // Add your state and logic here
+}
